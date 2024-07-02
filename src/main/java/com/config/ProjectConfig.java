06ffe25eb9a2125a0b6e869397a9adc95a18bcbe
glaxyt <glaxyt@naver.com>
2024-07-02 16:12:43 +0900

    커밋 테스트2

--- src/main/java/com/config/ProjectConfig.java
+++ src/main/java/com/config/ProjectConfig.java
@@ -1,5 +1,5 @@
 package com.config;
-// test
+// 124214
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.UserDetailsService;
